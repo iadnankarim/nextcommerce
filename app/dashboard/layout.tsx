aeb703522573fb0f38a2d57c4b42257fdf84dfc0
Adnan Karim <adnankarim725@gmail.com>
2025-10-26 20:12:49 +0500

    nextcommer full stack app

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,54 @@
+import { ReactNode } from 'react';
+import { DashboardNavigation } from '../components/dashboard/DashboardNavigation';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { Button } from '@/components/ui/button';
+import { CircleUser, MenuIcon } from 'lucide-react';
+import { DropdownMenu } from '@radix-ui/react-dropdown-menu';
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex w-full flex-col max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <header className="sticky top-0 flex  h-16 items-center justify-between gap-4 border-b border-gray-200 bg-white">
+        <nav className="hidden font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          <DashboardNavigation />
+        </nav>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button className="shrink md:hidden" variant="outline" size="icon">
+              <MenuIcon className="h-5 w-5" />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent side="left">
+            <nav className="flex flex-col gap-5 text-lg font-base mt-5">
+              <DashboardNavigation />
+            </nav>
+          </SheetContent>
+        </Sheet>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="rounded-full" variant="secondary" size="icon">
+              <CircleUser className="w-5 h-5" />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </header>
+      <main className="my-5">{children}</main>
+    </div>
+  );
+}
