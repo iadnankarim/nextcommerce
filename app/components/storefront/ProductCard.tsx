5639f91d10572d801e6edcb9a3a0b0eddf8bebac
Adnan Karim <adnankarim725@gmail.com>
2025-11-04 12:13:58 +0500

    Update ProductCard.tsx
    add featured product

--- app/components/storefront/ProductCard.tsx
+++ app/components/storefront/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import {
   Carousel,
   CarouselContent,
@@ -6,6 +7,7 @@ import {
   CarouselPrevious,
 } from '@/components/ui/carousel';
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface iAppProops {
   item: {
@@ -36,6 +38,17 @@ export function ProductCard({ item }: iAppProops) {
         <CarouselPrevious className="ml-16" />
         <CarouselNext className="mr-16" />
       </Carousel>
+
+      <div className="flex justify-between items-center  mt-2">
+        <h1 className="font-semibold text-xl">{item.name}</h1>
+        <h3 className="inline-flex items-center rounded-md bg-purple-300/30 px-2 py-1 text-xs font-medium ring-1 ring-inset ring-primary/10">
+          ${item.price}
+        </h3>
+      </div>
+      <p className="text-gray-600 text-sm mt-1 line-clamp-2 ">{item.description}</p>
+      <Button className="w-full mt-3 ">
+        <Link href={`/product/${item.id}`}>Learn More !!</Link>
+      </Button>
     </div>
   );
 }
