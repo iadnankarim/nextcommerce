a508b0b7f6c4707a8839d22bddfcbe39524508bc
Adnan Karim <adnankarim725@gmail.com>
2025-11-05 11:18:57 +0500

    navbar button
    navbar active button background color add

--- app/components/storefront/NavbarLink.tsx
+++ app/components/storefront/NavbarLink.tsx
@@ -1,4 +1,7 @@
+'use client';
+import { cn } from '@/lib/utils';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export const navbarLinks = [
   {
@@ -14,20 +17,28 @@ export const navbarLinks = [
   {
     id: 2,
     name: 'Men',
-    href: '/',
+    href: '/products/men',
   },
   {
     id: 3,
     name: 'Women',
-    href: '/',
+    href: '/products/women',
   },
 ];
 
 export function NavbarLinks() {
+  const location = usePathname();
   return (
     <div className="hidden md:flex justify-center items-center gap-x-4 ml-8">
       {navbarLinks.map((item) => (
-        <Link href={item.href} key={item.id}>
+        <Link
+          href={item.href}
+          key={item.id}
+          className={cn(
+            location === item.href ? 'bg-muted' : 'hover:bg-muted hover:bg-opacity-75',
+            'group p-2 font-medium rounded-md'
+          )}
+        >
           {item.name}
         </Link>
       ))}
